0eb156fd6d90230d23714fe7c91ccac5870b1441
99002459 <71481634+99002459@users.noreply.github.com>
2020-09-19 00:30:07 +0530

    Update shape_math.c

--- shapes_math/src/shape_math.c
+++ shapes_math/src/shape_math.c
@@ -2,41 +2,43 @@
 #include "shape_math.h"
 
 #define pi 3.14159
-float area, float perimeter;
-int rectangle(int len_rect, int bred_rect)
+
+int rectangle_area(int len_rect, int bred_rect)
 {
-   area=len_rect*bred_rect;
-   perimeter=2*(len_rect+bred_rect);
-   printf("Area of the rectangle= %d",area);
-   printf("Perimeter of the rectangle= %d",perimeter);
-   return 0;
+      return len_rect*bred_rect;
+}
+int rectangle_peri(int len_rect, int bred_rect)
+{
+      return 2*(len_rect+bred_rect);
 }
 
-int circle(int radius)
+int circle_area(int radius)
 {
-    area=pi*radius*radius;
-    perimeter=2*pi*radius;
-    printf("Area of the circle= %f",area);
-    printf("Perimeter of the circle= %f",perimeter);
-    return 0;
+     return pi*radius*radius;
 }
 
-int square(int side)
+int circle_perim(int radius)
 {
-    area=side*side;
-    perimeter=4*side;
-    printf("Area of the square= %d",area);
-    printf("Perimeter of the square= %d",perimeter);
-    return 0;
-
+     return 2*pi*radius;
+}
+int square_area(int side)
+{
+     return side*side;
 }
 
-int triangle(int side1,int side2, int side3)
+int square_peri(int side)
+{    
+    return 4*side;
+}
+int triangle_area(int side1,int side2, int side3)
 {
-    int semi,prod;
+    int semi,prod,area;
     semi=(side1+side2+side3)/2;
-    prod=semi*(semi-side1)*(semi-side2)*(semi-side3);
-    area=sqrt(prod);
-    perimeter=side1+side2+side3;
-    return 0;
+    prod=semi*(semi-side1)*(semi-side2)*(semi-side3); 
+    area=sqrt(prod)
+    return area;
+}
+int triangle_peri(int side1,int side2, int side3)
+{
+    return side1+side2+side3;
 }
