4673350d1d379843d5ee83d981f4cc4f38a8612d
99002459 <71481634+99002459@users.noreply.github.com>
2020-09-19 00:47:21 +0530

    Update main.c

--- shapes_math/main.c
+++ shapes_math/main.c
@@ -3,25 +3,51 @@
 
 int main()
 {
-    int number;
-    printf("Menu:\n");
-    printf("1. Calculations on Rectangle\n");
+    int number,len,bred,side,rad,side1,side2,side3;
+    float rect_area, rect_peri, circ_peri, circ_area,sqr_area,sqr_peri,tri_area, tri_peri;
+    /*printf("Menu:\n");
+    printf("1. Calculating area of Rectangle\n");
     printf("2. Calculations on Square\n");
     printf("3. Calculations on Circle\n");
     printf("4. Calculations on Triangle\n");
     printf("5. Exit\n");
     printf("Select option from the menu: \n");
+    */
      __fpurge(stdin);
     scanf("%d", &number);
 
     if(EXIT == number)
     {
-        printf("\nThank you. Exiting the Application\n");
         exit(0);
     }
 
     if(number>0 && number<8)
+    { 
+        switch(number)
     {
+        case 1:
+            scanf("%d %d", &len, &bred);
+            rect_area=rectangle_area(len,bred);
+            rect_peri=rectangle_peri(len,bred);    
+            break;
+        case 2:
+            scanf("%d ", &side);
+            sqr_area=square_area(side); 
+            sqr_peri=square_peri(side);
+            break;
+        case 3:
+            scanf("%d ", &rad);
+            circ_area=circle_area(rad);
+            circ_peri=circle_peri(rad);
+            break;
+        case 4:
+            scanf("%d %d %d", &side1, &side2, &side3);
+            tri_area=triangle_area(side1,side2,side3);
+            tri_peri=triangle_peri(side1,side2,side3);
+            break;
+        case 5:
+            exit(0);
+            break;
 
     }
     else
