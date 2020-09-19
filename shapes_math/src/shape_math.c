04ff4544913619b70d28764873f89834a4d46d03
99002459 <71481634+99002459@users.noreply.github.com>
2020-09-19 10:43:58 +0530

    Update shape_math.c

--- shapes_math/src/shape_math.c
+++ shapes_math/src/shape_math.c
@@ -1,6 +1,6 @@
 
 #include "shape_math.h"
-#include "operations.h"
+
 
 #define pi 3.14
 
@@ -43,3 +43,90 @@ int triangle_peri(int side1,int side2, int side3)
 {
     return side1+side2+side3;
 }
+
+
+int add(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int subtract(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int multiply(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int divide(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return operand1 / operand2;
+}
+
+int mod(operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return operand1 % operand2;
+}
+
+int is_prime(int operand1)
+{
+	int i;
+   for(i=2;i<operand1;i++)
+  /* checks prime number */
+  if(operand1%i== 0)
+    break;
+  /* Return 1 for prime number */
+  if(i>operand1/2)
+  return -1;
+  else
+  return 1;
+}
+
+int is_even_odd(int operand1)
+{
+	if(operand1%2==0)
+		return 1;
+	else
+		return 0;
+}
+
+int factorial(int operand1)
+{
+  /* Return -1 for negative numbers */
+  if(operand1 < 0)
+    return -1;
+
+  /* Return 1 for 0 */
+  if(operand1 == 0)
+    return 1;
+
+  /* Recursively calculate Factorial of the number */
+  return operand1 * factorial(operand1-1);
+}
+
+int positive(int operand1)
+{
+	if(operand1>0)
+		return 1;
+}
+
+int negative(int operand1)
+{
+	if(operand1<0)
+		return 0;
+}
+
+int zero(int operand1)
+{
+	if(operand1==0)
+		return 0;
+}
+
