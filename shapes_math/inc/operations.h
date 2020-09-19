154abb8dec56a1f806abd56378a096a822a8e93a
99002459 <71481634+99002459@users.noreply.github.com>
2020-09-19 10:13:01 +0530

    Update operations.h

--- shapes_math/inc/operations.h
+++ shapes_math/inc/operations.h
@@ -3,30 +3,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio_ext.h>
+
 
 
 int add(int operand1, int operand2);
-
 int subtract(int operand1, int operand2);
-
 int multiply(int operand1, int operand2);
-
 int divide(int operand1, int operand2);
-
-int prime(int operand1);
-
-int evenodd(int operand1);
-
+int mod(int operand1,int operand2);
+int is_prime(int operand1);
+int is_even_odd(int operand1);
 int factorial(int operand1);
-
 int positive(int operand1); 
-
 int negative(int operand1); 
-
 int zero(int operand1); 
 
-int remainder(int operand1,int operand2);
+
 
  
 
