ec949da82c5870e5b1e9eb40c12a9b9496e4d70f
99002459 <71481634+99002459@users.noreply.github.com>
2020-09-19 09:18:25 +0530

    Create operations.h

---
+++ shapes_math/inc/operations.h
@@ -0,0 +1,34 @@
+#ifndef OPERATIONS_H
+#define _OPERATIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+
+int add(int operand1, int operand2);
+
+int subtract(int operand1, int operand2);
+
+int multiply(int operand1, int operand2);
+
+int divide(int operand1, int operand2);
+
+int prime(int operand1);
+
+int evenodd(int operand1);
+
+int factorial(int operand1);
+
+int positive(int operand1); 
+
+int negative(int operand1); 
+
+int zero(int operand1); 
+
+int remainder(int operand1,int operand2);
+
+ 
+
+
+#endif 
